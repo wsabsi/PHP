6724783582c3ffd4fc33c6efae815e88432e5243
wsabsi <williansantana.angola@gmail.com>
2015-07-08 11:34:10 -0300

    Segundo commit. Bullshit removido

--- index.php
+++ index.php
@@ -1,40 +1,10 @@
 <html> 
 <title>PHP from Scratch</title>
 <h2>My first PHP website</h2>
-<body>
-	
-	<?php                                                  
-		     
+<?php                                                  	     
 		                                              
-		$array = ["Jose", "Maria","Joao"];                 
-		                                                   
-		foreach ($array as $key) {                         
-				                                           
-			echo "<li>$key<li/>";                          
-			if ($key == "Joao") {                          
-				echo "<strong>Achamos o Joao!</strong>";   
-			}else{                                         
-				echo "Ainda nao achamos o Joao.";          
-			}                                            
-		}     
-		echo "Data de hoje: ".date('d/m/Y'); 
-        echo "E agora sao".date('H:i:s');                          
-		                  
-	?>                                                     
-</body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+	echo "Data de hoje: ".date('d/m/Y'); 
+    echo "E agora sao".date('H:i:s');                          
+?>                                                     
 </html>  
